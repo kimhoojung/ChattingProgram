868824b4ea0d1110b617f4bde74c8b1cb3e82a7f
DESKTOP-SV7LVDR\김호정 <hojung9927h@gmail.com>
2022-01-12 12:44:00 +0900

    TCPClient추가

---
+++ src/chat/TCPClient.java
@@ -0,0 +1,33 @@
+package chat;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+public class TCPClient {
+    private static final String SERVER_IP = "192.168.35.229";
+    private static final int SERVER_PORT = 5000;
+
+    public static void main(String[] args) {
+        Socket socket  = null;
+
+        try{
+            socket = new Socket();
+            socket.connect(new InetSocketAddress(SERVER_IP, SERVER_PORT));
+
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally{
+            try{
+                if( socket != null && !socket.isClosed()){
+                    socket.close();
+                }
+            }
+            catch(IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
