b5b0757a447ad516a44f819bcbdd5528efb129ab
DESKTOP-SV7LVDR\김호정 <hojung9927h@gmail.com>
2022-01-14 16:41:15 +0900

    TCPClient 수정

--- src/chat/TCPClient.java
+++ src/chat/TCPClient.java
@@ -5,7 +5,7 @@ import java.net.InetSocketAddress;
 import java.net.Socket;
 
 public class TCPClient {
-    private static final String SERVER_IP = "192.168.35.229";
+    private static final String SERVER_IP = "192.168.123.121";
     private static final int SERVER_PORT = 5000;
 
     public static void main(String[] args) {
@@ -14,7 +14,7 @@ public class TCPClient {
         try{
             socket = new Socket();
             socket.connect(new InetSocketAddress(SERVER_IP, SERVER_PORT));
-
+            System.out.println("TCPClient 연결성공");
         }
         catch (IOException e) {
             e.printStackTrace();
